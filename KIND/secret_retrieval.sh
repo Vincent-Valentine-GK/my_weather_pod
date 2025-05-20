67270dd37e98415762b93ecc9c3bec849a831858
Ray Thomas <ray.thomas@gmail.com>
2025-05-20 08:47:50 -0500

    Adding more bash scripts

--- KIND/secret_retrieval.sh
+++ KIND/secret_retrieval.sh
@@ -1,1 +1,11 @@
 # For zsh
+
+# Grabs keystore and past to clibboard (Converts to binary)
+function oc_grab_trustore() {
+    oc get secret "$1" -o jsonpath='{.data.trustore}' | base64 --decode | pbcopy }
+}
+
+# Grabs keystore and past to clibboard
+function oc_grab_trustore() {
+    oc get secret "$1" -o jsonpath='{.data.trustore}' | base64 | pbcopy }
+}
