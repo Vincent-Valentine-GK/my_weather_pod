5cf3cbbfe3465fbc2c22a9dde5df0f8a8712ec09
ThatDude_Vinny <vincent_valentine@outlook.com>
2025-05-20 09:18:02 -0500

    Adding shell commands

--- KIND/secret_retrieval.sh
+++ KIND/secret_retrieval.sh
@@ -1,11 +1,14 @@
 # For zsh
 
-# Grabs keystore and past to clibboard (Converts to binary)
+# Grabs keystore and past to clipboard
 function oc_grab_trustore() {
     oc get secret "$1" -o jsonpath='{.data.trustore}' | base64 --decode | pbcopy }
+    kubectl get secret $1 --output="jsonpath={.data.\.dockerconfigjson}" | base64 --decode | pbcopy # For MAC
+    kubectl get secret $1 --output="jsonpath={.data.\.dockerconfigjson}" | base64 --decode | xclip -selection clipboard # For Linux
+    
 }
 
-# Grabs keystore and past to clibboard
+# Grabs keystore and past to clibboard  (Converts to binary)
 function oc_grab_trustore() {
     oc get secret "$1" -o jsonpath='{.data.trustore}' | base64 | pbcopy }
 }
